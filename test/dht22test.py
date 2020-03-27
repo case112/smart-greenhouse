7f0edd445455b0fdffcc1c45d8912bd5be7c97d8
Oskar <case112@users.noreply.github.com>
2020-03-27 09:01:58 +0200

    Create dht22test.py

---
+++ test/dht22test.py
@@ -0,0 +1,33 @@
+import time
+import board
+import adafruit_dht
+ 
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D27)
+
+counter = 0
+avg_temp = 0
+ 
+while counter < 10:
+    try:
+        # Print the values to the serial port
+        temperature = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        counter += 1
+        avg_temp += temperature
+        
+        print(
+            "Temp: {:.1f} C    Humidity: {}% ".format(
+                temperature, humidity
+            )
+        )
+ 
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+ 
+    time.sleep(5.0)
+
+print("Average temp: {:.1f} C ".format(avg_temp))
+
+
