c4f7ebff56f27e32c78778083be0497847f927b4
Oskar <case112@users.noreply.github.com>
2020-04-02 19:22:10 +0300

    Updated csv_writer

--- dht22/csv_writer.py
+++ dht22/csv_writer.py
@@ -2,13 +2,21 @@ import csv
 import time
 import board
 import adafruit_dht
-from get_data import get_data
+from dht22_measure import get_dht22_data
 
 def write_to_file(file_name, dht22_device, location):
 
     with open(file_name, 'a+', newline='') as csvfile:
         datawriter = csv.writer(csvfile)
-        data = get_data(dht22_device, location)         
-        datawriter.writerow(data)
-    
 
+        token = False
+        while not token:
+            try:
+                data = get_dht22_data(dht22_device, location)
+                datawriter.writerow(data)
+                token = True
+        
+            except RuntimeError as error:
+                print(error.args[0])
+
+            time.sleep(2.0)
--- dht22/get_all.py
+++ dht22/get_all.py
@@ -4,13 +4,13 @@ from get_data import get_data
 
 
 #Greenhouse sensor1
-#get_data(adafruit_dht.DHT22(board.D17), 'Greenhouse1')
+get_data(adafruit_dht.DHT22(board.D17), 'Greenhouse1')
 
 #Greenhouse sensor2
-#get_data(adafruit_dht.DHT22(board.D22), 'Greenhouse2')
+get_data(adafruit_dht.DHT22(board.D22), 'Greenhouse2')
 
 #Greenhouse cabinet sensor
-#get_data(adafruit_dht.DHT22(board.D27), 'Cabinet')
+get_data(adafruit_dht.DHT22(board.D27), 'Cabinet')
 
 #Outside sensor
 get_data(adafruit_dht.DHT22(board.D18), 'Outside')
