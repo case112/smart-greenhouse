59a08d39418688246f929c4ede6e4eb0ac77176d
Oskar <case112@users.noreply.github.com>
2020-04-02 18:22:17 +0300

    Created get_data.py to handle runtime errors

--- dht22/get_all.py
+++ dht22/get_all.py
@@ -1,15 +1,16 @@
 import board
 import adafruit_dht
-from dht22_measure import get_dht22_data
+from get_data import get_data
+
 
 #Greenhouse sensor1
-get_dht22_data(adafruit_dht.DHT22(board.D17), 'Greenhouse1')
+#get_data(adafruit_dht.DHT22(board.D17), 'Greenhouse1')
 
 #Greenhouse sensor2
-get_dht22_data(adafruit_dht.DHT22(board.D22), 'Greenhouse2')
+#get_data(adafruit_dht.DHT22(board.D22), 'Greenhouse2')
 
 #Greenhouse cabinet sensor
-get_dht22_data(adafruit_dht.DHT22(board.D27), 'Cabinet')
+#get_data(adafruit_dht.DHT22(board.D27), 'Cabinet')
 
 #Outside sensor
-get_dht22_data(adafruit_dht.DHT22(board.D18), 'Outside')
+get_data(adafruit_dht.DHT22(board.D18), 'Outside')
---
+++ dht22/get_data.py
@@ -0,0 +1,19 @@
+import board
+import adafruit_dht
+from dht22_measure import get_dht22_data
+
+def get_data(device, location):
+
+    token = False
+
+    # Try to take measurement multiple times in case of RuntimeError
+    while not token:
+        try:
+            get_dht22_data(device, location)
+            token = True
+            
+        except RuntimeError as error:
+            # Errors happen fairly often, DHT's are hard to read, just keep going
+            print(error.args[0])
+
+        time.sleep(2.0)
